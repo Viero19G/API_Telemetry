e45ca17822f52b63153d1123a20d1e91d73b9ac5
ti3 <ti3@robustec.com.br>
2025-08-07 08:44:50 -0300

    Created model for expected Json Data

--- api_iot_py/models.py
+++ api_iot_py/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+"""Classe estruturada e pensada para receber o JSON
+{
+  "id":  "robustec0001", 
+  "state": "stop",
+  "timestamp": 1754510232, 
+  "hours_worked":  1234.56
+}
+
+Sendo que "state" pode receber:
+- start
+- stop
+- running
+- stopped"""
+
+class IoTEvent(models.Model):
+    DEVICE_STATE_CHOICES = [
+        ('start', 'Start'),
+        ('stop', 'Stop'),
+        ('running', 'Running'),
+        ('stopped', 'Stopped'),
+    ]
+
+    device_id = models.CharField(max_length=100)  # vem do campo "id" no JSON
+    state = models.CharField(max_length=10, choices=DEVICE_STATE_CHOICES)
+    timestamp = models.DateTimeField()  # será convertido antes de salvar
+    hours_worked = models.FloatField()
+
+    def __str__(self):
+        return f"{self.device_id} - {self.state} @ {self.timestamp}"
