677056c70872816782d1b53869b9452d118e8667
ti3 <ti3@robustec.com.br>
2025-08-07 09:50:02 -0300

    Create Routes for CRUD  endpoints

--- api_iot_py/urls.py
+++ api_iot_py/urls.py
@@ -1,11 +1,20 @@
 # api_iot_py/urls.py
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import IoTEventViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+router = DefaultRouter()
+router.register(r'iot-events', IoTEventViewSet, basename='iot-event')
+
 urlpatterns = [
+    # JWT Auth
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # CRUD endpoints
+    path('', include(router.urls)),
 ]
