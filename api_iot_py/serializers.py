e7c6315cc508a205073b0f9fcfd19c1b2f2919b1
ti3 <ti3@robustec.com.br>
2025-08-07 09:32:22 -0300

    Create a Serializer to receive POST methods of the IoTEvent

---
+++ api_iot_py/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import IoTEvent
+from datetime import datetime
+
+class IoTEventSerializer(serializers.ModelSerializer):
+    # Campo extra para receber o timestamp como UNIX (segundos)
+    timestamp_unix = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = IoTEvent
+        fields = ['id', 'device_id', 'state', 'timestamp', 'hours_worked', 'timestamp_unix']
+        read_only_fields = ['timestamp']  # timestamp calculado, não editável diretamente
+
+    def create(self, validated_data):
+        # Extrai e converte timestamp_unix em datetime
+        timestamp_unix = validated_data.pop('timestamp_unix')
+        validated_data['timestamp'] = datetime.fromtimestamp(timestamp_unix)
+        return super().create(validated_data)
